f419ab133470dced77bd313e9d985cb129d693b6
Cyanide-zh <111837046+Cyanide-zh@users.noreply.github.com>
2024-12-03 18:54:13 +0800

    Update get-sources.sh

--- get-sources.sh
+++ get-sources.sh
@@ -9,11 +9,12 @@ tar -xvzf openssl-1.0.2j.tar.gz
 # -------------------------
 
 # Branch to checkout from Android source code repo
-branch=android-7.1.2_r33
+#branch=android-7.1.2_r33
+branch=android15-release
 
 # DOWNLOAD necessary files
 # -------------------------
-echo "\n>> >>> ADB for ARM <<< \n"
+echo "\n>> >>> ADB for Windows <<< \n"
 echo "\n>> Downloading necessay files ($branch branch)\n"
 mkdir android-adb
 cd android-adb
@@ -27,8 +28,8 @@ mkdir external
 cd external
 git clone -b $branch https://android.googlesource.com/platform/external/zlib
 git clone -b $branch https://android.googlesource.com/platform/external/zopfli/
-git clone -b $branch https://android.googlesource.com/platform/external/safe-iop/
-git clone -b $branch https://android.googlesource.com/platform/external/gtest/
+git clone -b main https://android.googlesource.com/platform/external/safe-iop/ #use the main branch
+git clone -b main  https://android.googlesource.com/platform/external/gtest/ #use the main branch
 cd ..
 mkdir platform
 cd platform
